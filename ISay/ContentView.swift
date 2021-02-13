44f13f544fea4aaaeb8cb0f2d3596dd9c6ef279c
hugepower <yezicccccc@gmail.com>
2021-02-13 14:04:02 +0800

    Initial Commit

---
+++ ISay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ISay
+//
+//  Created by 叶风 on 2021/2/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ISay/ISayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISayApp.swift
+//  ISay
+//
+//  Created by 叶风 on 2021/2/13.
+//
+
+import SwiftUI
+
+@main
+struct ISayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
